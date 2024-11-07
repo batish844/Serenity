d4e525a9c9deab431a01796fe058b06e76345f41
batish844 <batish844@gmail.com>
2024-11-07 02:35:57 +0200

    Finalized Categories Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,40 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {fontFamily: {
+      billion: ['"The Billion"', 'serif'],
+      width: {
+        '161px': '161px',
+        '428px': '428px',
+        '429px': '429px',
+        '436px': '436px',
+      },
+      height: {
+        '42px': '42px',
+        '51px': '51px',
+        '123px': '123px',
+        '455px': '455px',
+      },
+      borderRadius: {
+        '10px': '10px',
+        '44px': '44px',
+      },
+      fontSize: {
+        '17px': '17px',
+        '30px': '30px',
+        '36px': '36px',
+        '74px': '74px',
+      },
+      inset: {
+        '46px': '46px',
+        '66px': '66px',
+        '83px': '83px',
+        '332px': '332px',
+        '365px': '365px',
+        '373px': '373px',
+      },
+    },
+  },
   },
   plugins: [],
 }
