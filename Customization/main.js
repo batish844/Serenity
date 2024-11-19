c942c15c79b1e5c6c77c2d7157609e596b2461c7
batish844 <batish844@gmail.com>
2024-11-20 00:19:27 +0200

    Finalized Customization Section

--- Customization/main.js
+++ Customization/main.js
@@ -1,4 +1,22 @@
 $(document).ready(function() {
+let menu = document.getElementById('mobile-menu');
+let toggleButton = document.getElementById('menu-toggle');
+let closeButton = document.getElementById('menu-close');
+
+toggleButton.addEventListener('click', () => {
+  menu.classList.remove('hidden'); 
+});
+
+closeButton.addEventListener('click', () => {
+  menu.classList.add('hidden'); 
+});
+
+menu.addEventListener('click', (e) => {
+  if (e.target === menu) {
+    menu.classList.add('hidden');
+  }
+});
+
     function setupFragranceSelection(fragranceName) {
         let selectedButton = null;
         $('h2').each(function() {
