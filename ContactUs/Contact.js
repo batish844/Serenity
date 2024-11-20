60b6482a6a0c6444fa5d69225696ffaf1a6ff830
ChaficAchour <shafi2fawaz2018@gmail.com>
2024-11-20 02:52:44 +0200

    Update comment

--- ContactUs/Contact.js
+++ ContactUs/Contact.js
@@ -3,7 +3,7 @@ document.getElementById('contactForm').addEventListener('submit', function(e) {
     const formData = new FormData(this);
     const formObject = Object.fromEntries(formData.entries());
     console.log('Form submitted:', formObject);
-    // Here you would typically send the data to a server
+    // Send data 
     alert('Thank you for your message. We will get back to you soon!');
     this.reset();
 });
