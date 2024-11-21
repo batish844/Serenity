91f0a42912fb5278f31c90fee119336b4376a42c
ChaficAchour <shafi2fawaz2018@gmail.com>
2024-11-21 21:20:15 +0200

    Part 2 Contact Us + Iphone Responsive

--- ContactUs/Contact.js
+++ ContactUs/Contact.js
@@ -1,9 +1,17 @@
-document.getElementById('contactForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    const formData = new FormData(this);
-    const formObject = Object.fromEntries(formData.entries());
-    console.log('Form submitted:', formObject);
-    // Send data 
-    alert('Thank you for your message. We will get back to you soon!');
-    this.reset();
+
+
+// Show answers
+document.querySelectorAll('.Quick-help-button').forEach(button => {
+    button.addEventListener('click', () => {
+        const answerContainer = button.closest('.Quick-help-Question').querySelector('.Quick-help-Answer');
+        
+        if (answerContainer.style.maxHeight && answerContainer.style.maxHeight !== '0px') {
+            answerContainer.style.maxHeight = '0';
+            button.textContent = '+';
+        } else {
+            answerContainer.style.maxHeight = answerContainer.scrollHeight + 'px'; // take the auto height
+            button.textContent = '−';
+        }
+    });
 });
+
