dde0c40be5ae8b2eb97c29948fed34e7561ae8ae
ChaficAchour <shafi2fawaz2018@gmail.com>
2024-11-30 15:57:27 +0200

    Some Updates

--- Cart/Cart.js
+++ Cart/Cart.js
@@ -1,3 +1,25 @@
+
+// $(document).ready(function () {
+//   let menu = document.getElementById("mobile-menu");
+//   let toggleButton = document.getElementById("menu-toggle");
+//   let closeButton = document.getElementById("menu-close");
+
+//   toggleButton.addEventListener("click", () => {
+//     menu.classList.remove("hidden");
+//   });
+
+//   closeButton.addEventListener("click", () => {
+//     menu.classList.add("hidden");
+//   });
+
+//   menu.addEventListener("click", (e) => {
+//     if (e.target === menu) {
+//       menu.classList.add("hidden");
+//     }
+//   });
+// });
+
+
 let cartItems = [];
 let subtotal = 0;
 let promoApplied = false;
